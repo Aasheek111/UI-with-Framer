9f342f98b10f1c7935a8dd0a7ec2445ec1e03b6d
Aashik Gautam <technicalaashik111@gmail.com>
2025-11-21 14:45:11 +0545

    half

--- ui/src/app/components/Hooks.tsx
+++ ui/src/app/components/Hooks.tsx
@@ -4,38 +4,75 @@ import useFeature, { Feature } from "../hooks/useFeature";
 import { useScroll } from "motion/react";
 import {
   motion,
+  useMotionTemplate,
   useMotionValue,
   useMotionValueEvent,
+  useSpring,
   useTransform,
 } from "framer-motion";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+
 function Hooks() {
   const features: Feature[] = useFeature();
-  const ref = useRef<HTMLDivElement>(null);
+  const ref1 = useRef<HTMLDivElement>(null);
+  const ref2=useRef<HTMLDivElement>(null);
+
   const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start end", "end start"], //yedi thyo ref ko element ko start viewport ko end ma aaye start ani start ma aaye end
+    target: ref2,
+    offset: ["start start", "end end"], //yedi thyo ref ko element ko start viewport ko end ma aaye start ani start ma aaye end
   });
 
-  const translate = useTransform(scrollYProgress, [0, 1], [200, -100]);
-  const opacity = useTransform(scrollYProgress, [0, 0.5, 1], [0, 1, 0]);
+  const translate = useSpring( useTransform(scrollYProgress, [0, 1], [200, -100]),{
 
-  useMotionValueEvent(scrollYProgress, "change", (latest) => {});
+    stiffness:100,
+    damping:30,
+    mass:1
+  });
+  const opacity = useTransform(scrollYProgress, [0, 0.5, 1], [0, 1, 0]);
+  const blur = useTransform(scrollYProgress, [0.5, 1], [0, 1]);
+  const scale = useTransform(scrollYProgress, [0, 1], [1.2, 0.8]);
+const background=["#1b263b","#415a77" ,"#778da9"]
+
+
+useMotionValueEvent(scrollYProgress,'change',(latest)=>{
+  console.log(latest)
+  if(latest>=0 && latest<0.33){
+    setBg(background[Math.floor(latest*background.length)])
+  }
+  else if(latest>0.33&& latest<0.66){
+    setBg(background[Math.floor(latest*background.length)])
+  }
+  else if(latest>0.66 && latest<=1){
+    setBg(background[Math.floor(latest*background.length)])
+  }
+  else{
+    setBg("gray")
+  }
+
+
+})
+const [bg,setBg]=useState<string>("#1b263b");
 
   return (
-    <div className=" flex min-h-screen bg-black justify-center items-center">
-      <div className="flex flex-col gap-10  max-w-4xl mx-auto py-10 " ref={ref}>
+    <div className=" flex min-h-screen bg-black justify-center items-center" style={{background:bg}} ref={ref2}>
+      <div className="flex flex-col gap-10  max-w-4xl mx-auto py-10 " ref={ref1}>
         {features.map((feature: Feature, index: number) => (
           <div
             key={index}
             className="grid grid-cols-2 gap-20 items-center py-40"
           >
-            <div className=" flex flex-col gap-5">
+            <motion.div
+              className=" flex flex-col gap-5"
+              style={{
+                filter: useMotionTemplate`blur(${blur}px)`,
+                scale: scale,
+              }}
+            >
               {feature.icon}
 
               <h1 className="text-white text-2xl font-bold">{feature.title}</h1>
               <p className="text-neutral-500">{feature.description}</p>
-            </div>
+            </motion.div>
             <motion.div
               style={{
                 y: translate,
--- ui/src/app/hooks/useFeature.tsx
+++ ui/src/app/hooks/useFeature.tsx
@@ -35,6 +35,7 @@ function useFeature():Feature[] {
       image:
         "https://images.unsplash.com/photo-1669023414180-4dcf35d943e1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGNvZGluZyUyMGZhc3R8ZW58MHx8MHx8fDA%3D",
     },
+   
   ];
   return features;
 }
