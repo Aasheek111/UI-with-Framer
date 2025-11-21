776281dcf55f5cc1a1eb8bfa71ef52fbc537fac6
Aashik Gautam <technicalaashik111@gmail.com>
2025-11-21 11:51:24 +0545

    features

--- ui/src/app/components/Hooks.tsx
+++ ui/src/app/components/Hooks.tsx
@@ -1,19 +1,54 @@
-import React, { useEffect } from "react";
-import { useAnimate } from "motion/react";
-
+"use client";
+import Image from "next/image";
+import { Fa500Px, FaReact, FaCode, FaFire } from "react-icons/fa";
 function Hooks() {
-  const [scope, animate] = useAnimate();
+  type Feature = {
+    icon: React.ReactNode;
+    title: string;
+    description: string;
+    image: string;
+  };
 
-  useEffect(() => {
-    animate(".useanimate", { opacity: 1 }, { duration:2 });
-  }, []);
+  const features: Feature[] = [
+    {
+      icon: <FaReact />,
+      title: "Lightning Fast",
+      description: "Built with smooth, optimized performance.",
+      image:
+        "https://images.unsplash.com/photo-1581677996915-0bec0bb3a1a1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZmFzdCUyMGNvZGV8ZW58MHx8MHx8fDA%3D",
+    },
+    {
+      icon: <FaCode />,
+      title: "Developer Friendly",
+      description: "Clean structure, easy to extend, joy to build.",
+      image:
+        "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29kZXxlbnwwfHwwfHx8MA%3D%3D",
+    },
+    {
+      icon: <FaFire />,
+      title: "Always Hot",
+      description: "Fresh updates and spicy improvements.",
+      image:
+        "https://images.unsplash.com/photo-1669023414180-4dcf35d943e1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGNvZGluZyUyMGZhc3R8ZW58MHx8MHx8fDA%3D",
+    },
+  ];
 
   return (
-    <div ref={scope} className="  h-screen w-full bg-amber-400 flex justify-center items-center">
-      <div className="useanimate  opacity-0 h-50 w-50 bg-black rounded-4xl text-4xl ">
+    <div className=" flex min-h-screen bg-black justify-center items-center">
+      <div className="flex flex-col gap-10 ">
+        {features.map((feature: Feature, index: number) => (
+          <div key={index} className="grid grid-cols-2 gap-20">
+            <div>
+              {feature.icon}
 
-
-        HEllo
+              {feature.description}
+              {feature.title}
+            </div>
+            <div>
+              <Image src={feature.image} alt="pic" height={500} className="w-100" width={500} />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
