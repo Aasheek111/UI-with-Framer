3ca245688531888b615b88642e61f940e58373fa
Aashik Gautam <technicalaashik111@gmail.com>
2025-12-01 16:45:13 +0545

    done

--- ui/src/app/components/AnimationSequence.tsx
+++ ui/src/app/components/AnimationSequence.tsx
@@ -1,31 +1,53 @@
 "use client";
 import { useAnimate, motion } from "motion/react";
-
+import { GrStatusGood } from "react-icons/gr";
 import React from "react";
 
 function AnimationSequence() {
   const [scope, animate] = useAnimate();
 
-  const startAnimating = async() => {
+  const startAnimating = async () => {
     animate(
-      "button",
-      { width: "5rem",borderRadius:'50rem'},
+      "span",
+      { display: "none" },
       {
-        duration: 0.3,
+        duration: 0,
       }
     );
     await animate(
+      ".load",
+      {
+        opacity: 1,
+      },
+      {
+        duration: 0.8,
+      }
+    );
+
+    await animate(".load", {
+      opacity: 0,
+    });
+    await animate(
       "button",
-      { },
+      { width: "5rem", borderRadius: "50rem" },
+      {
+        duration: 0.2,
+      }
+    );
+
+    await animate(
+      "button",
+      { scale: [1, 1.2, 0.8, 1], background: "#1CD62E" },
       {
         duration: 0.3,
       }
     );
-    animate(
-      "span",
-      { display:'none' },
+
+    await animate(
+      ".tick",
+      { opacity: 1 },
       {
-        duration: 0.1,
+        duration: 0.3,
       }
     );
   };
@@ -37,9 +59,14 @@ function AnimationSequence() {
       <motion.button
         style={{ width: "30rem" }}
         onClick={() => startAnimating()}
-        className=" h-20 rounded-lg bg-purple-700  text-white text-xl cursor-pointer "
+        className=" h-20 rounded-lg bg-purple-700 relative text-white text-xl cursor-pointer  flex justify-center items-center"
       >
         <motion.span>Purchase Now(135$)</motion.span>
+        <div className="tick absolute opacity-0 inset-0 p-1">
+          <GrStatusGood className="p-1" size={70} />
+        </div>
+
+        <div className=" load absoulte inset-0 opacity-0">Loading.....</div>
       </motion.button>
     </div>
   );
